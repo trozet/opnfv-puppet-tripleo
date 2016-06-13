5a3da87cf10d4ffa4b6d2587dbed50417cad59dc
Dan Radez <dradez@redhat.com>
2016-06-13 16:21:12 -0400

    adding tacker to load balancer
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- manifests/loadbalancer.pp
+++ manifests/loadbalancer.pp
@@ -174,6 +174,11 @@
 #  When set, enables SSL on the Sahara public API endpoint using the specified file.
 #  Defaults to undef
 #
+# [*tacker_certificate*]
+#  Filename of an HAProxy-compatible certificate and key file
+#  When set, enables SSL on the Tacker public API endpoint using the specified file.
+#  Defaults to undef
+#
 # [*trove_certificate*]
 #  Filename of an HAProxy-compatible certificate and key file
 #  When set, enables SSL on the Trove public API endpoint using the specified file.
@@ -281,6 +286,10 @@
 #  (optional) Enable or not Swift API binding
 #  Defaults to false
 #
+# [*tacker*]
+#  (optional) Enable or not Tacker API binding
+#  Defaults to false
+#
 # [*heat_api*]
 #  (optional) Enable or not Heat API binding
 #  Defaults to false
@@ -368,6 +377,8 @@
 #    'sahara_api_ssl_port' (Defaults to 13386)
 #    'swift_proxy_port' (Defaults to 8080)
 #    'swift_proxy_ssl_port' (Defaults to 13808)
+#    'tacker_api_port' (Defaults to 8888)
+#    'tacker_api_ssl_port' (Defaults to 8443)
 #    'trove_api_port' (Defaults to 8779)
 #    'trove_api_ssl_port' (Defaults to 13779)
 #  Defaults to {}
@@ -404,6 +415,7 @@ class tripleo::loadbalancer (
   $neutron_certificate       = undef,
   $cinder_certificate        = undef,
   $sahara_certificate        = undef,
+  $tacker_certificate       = undef,
   $trove_certificate         = undef,
   $manila_certificate        = undef,
   $glance_certificate        = undef,
@@ -422,6 +434,7 @@ class tripleo::loadbalancer (
   $cinder                    = false,
   $congress                  = false,
   $sahara                    = false,
+  $tacker                    = false,
   $trove                     = false,
   $manila                    = false,
   $glance_api                = false,
@@ -488,6 +501,8 @@ class tripleo::loadbalancer (
     sahara_api_ssl_port => 13386,
     swift_proxy_port => 8080,
     swift_proxy_ssl_port => 13808,
+    tacker_api_port => 8888,
+    tacker_api_ssl_port => 8443,
     trove_api_port => 8779,
     trove_api_ssl_port => 13779,
   }
@@ -608,6 +623,11 @@ class tripleo::loadbalancer (
     $sahara_bind_certificate = $sahara_certificate
   } else {
     $sahara_bind_certificate = $service_certificate
+  }
+  if $tacker_certificate {
+    $tacker_bind_certificate = $tacker_certificate
+  } else {
+    $tacker_bind_certificate = $service_certificate
   }
   if $trove_certificate {
     $trove_bind_certificate = $trove_certificate
@@ -759,6 +779,19 @@ class tripleo::loadbalancer (
     $sahara_bind_opts = {
       "${sahara_api_vip}:${ports[sahara_api_port]}" => $haproxy_listen_bind_param,
       "${public_virtual_ip}:${ports[sahara_api_port]}" => $haproxy_listen_bind_param,
+    }
+  }
+
+  $tacker_api_vip = hiera('tacker_api_vip', $controller_virtual_ip)
+  if $tacker_bind_certificate {
+    $tacker_bind_opts = {
+      "${tacker_api_vip}:${ports[tacker_api_port]}" => $haproxy_listen_bind_param,
+      "${public_virtual_ip}:${ports[tacker_api_ssl_port]}" => union($haproxy_listen_bind_param, ['ssl', 'crt', $tacker_bind_certificate]),
+    }
+  } else {
+    $tacker_bind_opts = {
+      "${tacker_api_vip}:${ports[tacker_api_port]}" => $haproxy_listen_bind_param,
+      "${public_virtual_ip}:${ports[tacker_api_port]}" => $haproxy_listen_bind_param,
     }
   }
 
@@ -1484,12 +1517,27 @@ class tripleo::loadbalancer (
       },
       collect_exported => false,
     }
+
     haproxy::balancermember { 'congress':
       listening_service => 'congress',
       ports             => '1789',
       ipaddresses       => $controller_hosts_real,
       server_names      => $controller_hosts_names_real,
       options           => ['check', 'inter 2000', 'rise 2', 'fall 5'],
+    }
+  }
+
+  if $tacker {
+    haproxy::listen { 'tacker':
+      bind             => $tacker_bind_opts,
+      collect_exported => false,
+    }
+    haproxy::balancermember { 'tacker':
+      listening_service => 'tacker',
+      ports             => '8888',
+      ipaddresses       => hiera('tacker_api_node_ips', $controller_hosts_real),
+      server_names      => $controller_hosts_names_real,
+      options           => $haproxy_member_options,
     }
   }
 }
